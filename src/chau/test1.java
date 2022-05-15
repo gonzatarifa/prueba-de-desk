7d95c6575887184f3cf4982734991954b14210a5
gonzatarifa <gon17tarifa@gmail.com>
2022-05-15 14:37:58 -0300

    add class chau

--- src/chau/test1.java
+++ src/chau/test1.java
@@ -1,10 +1,10 @@
+package chau;
 
-public class test {
+public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola como te va");
-	
+		System.out.println("chau como te fue?");
 	}
 
 }
