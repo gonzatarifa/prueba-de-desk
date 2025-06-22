494dbdf1857cd85fe32359be94cec8b5197ef206
gonzatarifa <gon17tarifa@gmail.com>
2025-06-22 20:20:01 -0300

    s

--- src/negocio/oa.java
+++ src/negocio/oa.java
@@ -3,7 +3,7 @@ package negocio;
 public class oa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("chau blodel como te fue?");
+		System.out.println("chau blodel como te fue?"); 
 	}
 
 }
